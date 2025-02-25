4b6963ab68bad32483328fb7bb5ec22b3a635da1
ZihanWang314 <510642032wzh@gmail.com>
2025-02-25 23:30:51 +0000

    update

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from transformers import AutoConfig, AutoModel
 
-config_path = "r1-tiny-hf"
-config = AutoConfig.from_pretrained(config_path)
+config_path = "config/r1-tiny-hf"
+config = AutoConfig.from_pretrained(config_path, trust_remote_code=True)
 
-model = AutoModel.from_config(config)
+model = AutoModel.from_config(config, trust_remote_code=True)
 
 print(model)
+breakpoint()
