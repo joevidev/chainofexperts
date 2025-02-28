fa4307c5621fee1554ecbbd732bcc230d8e2d92a
ZihanWang314 <510642032wzh@gmail.com>
2025-02-28 08:44:40 -0600

    update

--- train.sh
+++ train.sh
@@ -11,25 +11,6 @@ set -x
 # shift 2
 
 export PYTHONPATH=/home/zihan/CoE:$PYTHONPATH
-# torchrun main.py \
-#     trainer.project_name=metamathqa-sft \
-#     trainer.experiment_name=metamathqa-sft-pythia-160m \
-#     data.train_files=data/metamathqa/train.parquet \
-#     data.val_files=data/metamathqa/test.parquet \
-#     data.truncation=left \
-#     +data.text_keys=['query','response'] \
-#     data.micro_batch_size_per_gpu=4 \
-#     model.partial_pretrain=config/models/pythia-160m \
-#     +model.from_config=true \
-#     +model.override_config.moe_sparsity=1 \
-#     +model.override_config.moe_granularity=1 \
-#     +model.override_config.moe_topk=1 \
-#     trainer.default_local_dir=output \
-#     trainer.total_epochs=1 \
-#     trainer.logger=['console','wandb'] \
-#     trainer.default_hdfs_dir=null $@
-#     # +fsdp=false \
-
 
 # torchrun main.py \
 #     trainer.project_name=metamathqa-sft \
@@ -105,3 +86,22 @@ torchrun main.py \
     trainer.total_epochs=1 \
     trainer.logger=['console','wandb'] \
     trainer.default_hdfs_dir=null $@
+
+torchrun main.py \
+    trainer.project_name=metamathqa-sft \
+    trainer.experiment_name=metamathqa-sft-pythia-160m \
+    data.train_files=data/metamathqa/train.parquet \
+    data.val_files=data/metamathqa/test.parquet \
+    data.truncation=left \
+    +data.text_keys=['query','response'] \
+    data.micro_batch_size_per_gpu=4 \
+    model.partial_pretrain=config/models/pythia-160m \
+    +model.from_config=true \
+    +model.override_config.moe_sparsity=1 \
+    +model.override_config.moe_granularity=1 \
+    +model.override_config.moe_topk=1 \
+    trainer.default_local_dir=output \
+    trainer.total_epochs=1 \
+    trainer.logger=['console','wandb'] \
+    trainer.default_hdfs_dir=null $@
+    # +fsdp=false \
