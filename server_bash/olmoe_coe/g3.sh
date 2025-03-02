6e107aaad932984585fda275227aeb817d710b5c
ZihanWang314 <510642032wzh@gmail.com>
2025-03-02 22:45:03 +0000

    update

--- server_bash/olmoe_coe/g3.sh
+++ server_bash/olmoe_coe/g3.sh
@@ -2,52 +2,55 @@
 set -x  # Print each command before execution
 
 export PYTHONPATH=/home/zihan/CoE:$PYTHONPATH
-export CUDA_VISIBLE_DEVICES=2
-export MASTER_PORT=29502
+export CUDA_VISIBLE_DEVICES=3
+export MASTER_PORT=29503
 
 # Define configuration combinations
 # Format: experiment_suffix:sparsity:granularity:topk
 general_name="mmqa-olmoe_coe-h1024"
 configs=(
-    # "itr-c-64epts-8topk-1iter-4lyr:64:8:1:4"
-    # "itr-c-8epts-8topk-1iter-4lyr:8:8:1:4"
-    # "itr-c-64epts-8topk-2iter-4lyr:64:8:2:4"
-    # "itr-c-8epts-8topk-2iter-4lyr:8:8:2:4"
-    # "itr-c-64epts-8topk-3iter-4lyr:64:8:3:4"
-    # "itr-c-8epts-8topk-3iter-4lyr:8:8:3:4"
-    # "itr-c-64epts-8topk-4iter-4lyr:64:8:4:4"
-    # "itr-c-8epts-8topk-4iter-4lyr:8:8:4:4"
+    "fl-64epts-4topk-2iter-4lyr:64:4:2:4"
+    "fl-64epts-2topk-4iter-4lyr:64:2:4:4"
+    "fl-64epts-1topk-8iter-4lyr:64:1:8:4"
+)
+    # # "itr-c-64epts-8topk-1iter-4lyr:64:8:1:4"
+    # # "itr-c-8epts-8topk-1iter-4lyr:8:8:1:4"
+    # # "itr-c-64epts-8topk-2iter-4lyr:64:8:2:4"
+    # # "itr-c-8epts-8topk-2iter-4lyr:8:8:2:4"
+    # # "itr-c-64epts-8topk-3iter-4lyr:64:8:3:4"
+    # # "itr-c-8epts-8topk-3iter-4lyr:8:8:3:4"
+    # # "itr-c-64epts-8topk-4iter-4lyr:64:8:4:4"
+    # # "itr-c-8epts-8topk-4iter-4lyr:8:8:4:4"
 
 
-    # "lyr-cm-64epts-8topk-1iter-2lyr:64:8:1:2"
-    # "lyr-cm-8epts-8topk-1iter-2lyr:8:8:1:2"
-    # "lyr-cm-64epts-8topk-1iter-4lyr:64:8:1:4"
-    # "lyr-cm-8epts-8topk-1iter-4lyr:8:8:1:4"
-    # "lyr-cm-64epts-8topk-1iter-6lyr:64:8:1:6"
-    # "lyr-cm-8epts-8topk-1iter-6lyr:8:8:1:6"
-    # "lyr-cm-64epts-8topk-1iter-8lyr:64:8:1:8"
-    # "lyr-cm-8epts-8topk-1iter-8lyr:8:8:1:8"
+    # # "lyr-cm-64epts-8topk-1iter-2lyr:64:8:1:2"
+    # # "lyr-cm-8epts-8topk-1iter-2lyr:8:8:1:2"
+    # # "lyr-cm-64epts-8topk-1iter-4lyr:64:8:1:4"
+    # # "lyr-cm-8epts-8topk-1iter-4lyr:8:8:1:4"
+    # # "lyr-cm-64epts-8topk-1iter-6lyr:64:8:1:6"
+    # # "lyr-cm-8epts-8topk-1iter-6lyr:8:8:1:6"
+    # # "lyr-cm-64epts-8topk-1iter-8lyr:64:8:1:8"
+    # # "lyr-cm-8epts-8topk-1iter-8lyr:8:8:1:8"
 
-    "lyr-m-64epts-16topk-1iter-2lyr:64:16:1:2"
-    "lyr-m-64epts-8topk-1iter-4lyr:64:8:1:4"
-    "lyr-m-64epts-6topk-1iter-6lyr:64:6:1:6"
-    "lyr-m-64epts-4topk-1iter-8lyr:64:4:1:8"
+    # # "lyr-m-64epts-16topk-1iter-2lyr:64:16:1:2"
+    # # "lyr-m-64epts-8topk-1iter-4lyr:64:8:1:4"
+    # # "lyr-m-64epts-6topk-1iter-6lyr:64:6:1:6"
+    # # "lyr-m-64epts-4topk-1iter-8lyr:64:4:1:8"
 
 
-    "wid-cm-32epts-4topk-1iter-4lyr:32:4:1:4"
-    "wid-cm-4epts-4topk-1iter-4lyr:4:4:1:4"
+    # # "wid-cm-32epts-4topk-1iter-4lyr:32:4:1:4"
+    # # "wid-cm-4epts-4topk-1iter-4lyr:4:4:1:4"
     # "wid-cm-64epts-8topk-1iter-4lyr:64:8:1:4"
     # "wid-cm-8epts-8topk-1iter-4lyr:8:8:1:4"
-    # "wid-cm-96epts-12topk-1iter-4lyr:96:12:1:4"
+    # "wid-cm-96epts-12topk-1iter-4lyr:96:12:1:4" # NOTE not done
     # "wid-cm-12epts-12topk-1iter-4lyr:12:12:1:4"
-    "wid-cm-128epts-16topk-1iter-4lyr:128:16:1:4"
-    "wid-cm-16epts-16topk-1iter-4lyr:16:16:1:4"
+    # # "wid-cm-128epts-16topk-1iter-4lyr:128:16:1:4"
+    # # "wid-cm-16epts-16topk-1iter-4lyr:16:16:1:4"
 
     # "wid-m-32epts-8topk-1iter-4lyr:32:8:1:4"
     # "wid-m-64epts-8topk-1iter-4lyr:64:8:1:4"
-    # "wid-m-96epts-8topk-1iter-4lyr:96:8:1:4"
-    # "wid-m-128epts-8topk-1iter-4lyr:128:8:1:4"
-)
+    # "wid-m-96epts-8topk-1iter-4lyr:96:8:1:4" # NOTE not done
+    # "wid-m-128epts-8topk-1iter-4lyr:128:8:1:4" # NOTE not done
 
 # Base command line arguments common to all runs
 base_args=(
